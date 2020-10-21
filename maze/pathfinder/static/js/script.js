971525caff2ed8ac0b395d538337631ac754feaf
Yibo Yan <totoroq@outlook.com>
2020-10-21 13:38:29 +0800

    Fix eventhandler firing issue

--- maze/pathfinder/static/js/script.js
+++ maze/pathfinder/static/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
     console.log("loaded");
     document.querySelectorAll('td').forEach(button => {
-        console.log('hello');
+        // console.log('hello');
         button.onclick = () => {
             console.log('Clicked');
         };
